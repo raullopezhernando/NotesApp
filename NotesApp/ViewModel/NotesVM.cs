b09d92d4cd734b3efee58fe4f9b9ff232b19d28f
Raúl López <raul.lopez@ingenoba.com>
2020-02-07 09:00:49 +0100

    Add project files.

---
+++ NotesApp/ViewModel/NotesVM.cs
@@ -0,0 +1,97 @@
+﻿using NotesApp.Model;
+using NotesApp.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.ViewModel
+{
+    public class NotesVM
+    {
+        public ObservableCollection<Notebook> Notebooks { get; set; }
+
+        private Notebook selectedNotebook;
+        public Notebook SelectedNotebook
+        {
+            get { return selectedNotebook; }
+            set
+            {
+                selectedNotebook = value;
+                //TODO: get notes
+            }
+        }
+
+        public ObservableCollection<Note> Notes { get; set; }
+
+        public NewNotebookCommand NewNotebookCommand { get; set; }
+        public NewNoteCommand NewNoteCommand { get; set; }
+
+        public NotesVM()
+        {
+            NewNotebookCommand = new NewNotebookCommand(this);
+            NewNoteCommand = new NewNoteCommand(this);
+
+            Notebooks = new ObservableCollection<Notebook>();
+            Notes = new ObservableCollection<Note>();
+
+            ReadNotebooks();
+        }
+
+        public void CreateNotebook()
+        {
+            Notebook newNotebook = new Notebook()
+            {
+                Name = "New notebook"
+            };
+
+            DatabaseHelper.Insert(newNotebook);
+        }
+
+        public void CreateNote(int notebookId)
+        {
+            Note newNote = new Note()
+            {
+                NotebookId = notebookId,
+                CreatedTime = DateTime.Now,
+                UpdatedTime = DateTime.Now,
+                Title = "New note"
+            };
+
+            DatabaseHelper.Insert(newNote);
+        }
+
+        public void ReadNotebooks()
+        {
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(DatabaseHelper.dbFile))
+            {
+                var notebooks = conn.Table<Notebook>().ToList();
+
+                Notebooks.Clear();
+                foreach(var notebook in notebooks)
+                {
+                    Notebooks.Add(notebook);
+                }
+            }
+        }
+
+        public void ReadNotes()
+        {
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(DatabaseHelper.dbFile))
+            {
+                if (SelectedNotebook != null)
+                {
+                    var notes = conn.Table<Note>().Where(n => n.NotebookId == SelectedNotebook.Id).ToList();
+
+                    Notes.Clear();
+                    foreach(var note in notes)
+                    {
+                        Notes.Add(note);
+                    }
+                }
+            }
+        }
+    }
+}
